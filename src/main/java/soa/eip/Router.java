b4b5fe3235198d071611a0aa8104dc1ed2b04ac8
Jorge Turbica <723883@unizar.es>
2020-12-16 23:03:39 +0100

    Request limit functionality added

--- src/main/java/soa/eip/Router.java
+++ src/main/java/soa/eip/Router.java
@@ -1,5 +1,7 @@
 package soa.eip;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +12,27 @@ public class Router extends RouteBuilder {
 
   @Override
   public void configure() {
+
+    Processor processor = new Processor() {
+      @Override
+      public void process(Exchange exchange) throws Exception {
+        String body = exchange.getIn().getBody(String.class);
+        // Comprobamos si existe el comando limitador de mensajes
+        if (body.matches("^.+ max:[0-9]+$")) {
+          String[] aux = body.split(":");
+          int count = Integer.parseInt(aux[aux.length - 1]);
+          body = body.replace("max:" + count, "");
+          // Se añade al final el limitador de cantidad
+          exchange.getIn().setBody(body + "?count=" + count);
+        }
+      }
+    };
+
     from(DIRECT_URI)
-      .log("Body contains \"${body}\"")
-      .log("Searching twitter for \"${body}\"!")
-      .toD("twitter-search:${body}")
-      .log("Body now contains the response from twitter:\n${body}");
+            .log("Body contains \"${body}\"")
+            .process(processor)
+            .log("Searching twitter for \"${body}\"!")
+            .toD("twitter-search:${body}")
+            .log("Body now contains the response from twitter:\n${body}");
   }
 }
